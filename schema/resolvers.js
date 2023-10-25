ac6358a1c19c9974ea95c8c739019a3d4c39f63b
Josh Sermon <jsermon@unknowna6a2f49b6b76.attlocal.net>
2023-10-25 18:58:52 -0400

    mutations - updating and removing  usernames and id

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -36,9 +36,28 @@ const resolvers = {
                user.id = lastId + 1;
                UserList.push(user);
                return user;
-           } 
-        }
+           },
+           
+           updateUsername: (parent, args) => {
+            // const id = args.input.id;
+            // const newUsername = args.input.newUsername
+            const { id, newUsername } = args.input; 
+            let userUpdated;
+            UserList.forEach((user) => {
+                if(user.id === Number(id)) {
+                    user.username = newUsername;
+                    userUpdated = user;
+                }
+            });
+            return userUpdated;
+            }, 
 
-};
+            deleteUser: (parent, args) => {
+                const id = args.id;
+                _.remove(UserList, (user) => user.id === Number(id));
+                return null;
+            }
+        },
+    };
 
 module.exports = { resolvers }
